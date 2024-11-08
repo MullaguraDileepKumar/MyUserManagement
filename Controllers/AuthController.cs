7303a4bfcc48f8e451cc62f5bf302549b203fb0f
Harshitha-JBS <hsr-c@tytonholdings.com>
2024-11-08 22:54:51 +0530

    first commit

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -8,5 +8,6 @@ namespace UserManagement.Controllers
     public class AuthController : ControllerBase
     {
         public string UserName {get;set;}
+        public string pwd {get;set;}
     }
 }
